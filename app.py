d1c2f43da044e71c8aaa00dc8409f3f0995e9ccc
vishwa dilshara <126236252+VishwaDilshara@users.noreply.github.com>
2023-08-07 11:10:39 +0530

    make delete

--- app.py
+++ app.py
@@ -1,3 +1,67 @@
+# from flask import Flask, render_template, request, redirect
+# from models import db, StudentModel
+#
+# app = Flask(__name__)
+#
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db.init_app(app)
+#
+# # Create the table before the first request
+# # @app._got_first_request
+# # def create_table():
+# #     db.create_all()
+#
+# @app.route('/create', methods = ['GET', 'POST'])
+# def create():
+#     if request.method == 'GET':
+#         return render_template('create.html')
+#
+#     if request.method == 'POST':
+#         hobby = request.form.getlist('hobbies')
+#         hobbies = ",".join(map(str, hobby))
+#         first_name = request.form['first_name']
+#         last_name = request.form['last_name']
+#         email = request.form['email']
+#         password = request.form['password']
+#         gender = request.form['gender']
+#         hobbies = hobbies
+#         country = request.form['country']
+#
+#         students = StudentModel(
+#             first_name = first_name,
+#             last_name = last_name,
+#             email = email,
+#             password = password,
+#             gender = gender,
+#             hobbies = hobbies,
+#             country = country
+#         )
+#         db.session.add(students)
+#         db.session.commit()
+#         return redirect('/')
+#
+# @app.route('/', methods = ['GET'])
+# def RetrieveList():
+#     students = StudentModel.query.all()
+#     return render_template('index.html', students = students)
+#
+# @app.route('/<int:id>/delete', methods=['GET', 'POST'])
+#
+# def delete(id):
+#     students = StudentModel.query.filter_by(id=id).first()
+#     if request.method == 'POST':
+#         if students:
+#             db.session.delete(students)
+#             db.session.commit()
+#             return redirect('/')
+#             abort(404)
+#         return render_template('delete.html')
+#
+#
+# app.run(host='localhost', port=8080)
+
+# -------------------------------------------------------------------------------
 from flask import Flask, render_template, request, redirect
 from models import db, StudentModel
 
@@ -8,11 +72,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 # Create the table before the first request
-# @app._got_first_request
-# def create_table():
-#     db.create_all()
+created_table = False
 
-@app.route('/create', methods = ['GET', 'POST'])
+@app.before_request
+def create_table():
+    global created_table
+    if not created_table:
+        db.create_all()
+        created_table = True
+
+@app.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'GET':
         return render_template('create.html')
@@ -29,92 +98,55 @@ def create():
         country = request.form['country']
 
         students = StudentModel(
-            first_name = first_name,
-            last_name = last_name,
-            email = email,
-            password = password,
-            gender = gender,
-            hobbies = hobbies,
-            country = country
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+            gender=gender,
+            hobbies=hobbies,
+            country=country
         )
         db.session.add(students)
         db.session.commit()
         return redirect('/')
     
-@app.route('/', methods = ['GET'])
+@app.route('/', methods=['GET'])
 def RetrieveList():
     students = StudentModel.query.all()
-    return render_template('index.html', students = students)
+    return render_template('index.html', students=students)
+#
+# @app.route('/<int:student_id>/delete', methods=['GET'])
+# def delete(student_id):
+#     student = StudentModel.query.get_or_404(student_id)
+#     db.session.delete(student)
+#     db.session.commit()
+#     return redirect('/')
+#     abort(404)
+#
+#
+#
+# def delete(id):
+#     students = StudentModel.query.filter_by(id=id).first()
+#     if request.method == 'POST':
+#         if students:
+#             db.session.delete(students)
+#             db.session.commit()
+#             return redirect('/')
+#             abort(404)
+#         return render_template('delete.html')
+@app.route('/<int:student_id>/delete', methods=['GET'])
+def delete(student_id):
+    student = StudentModel.query.get_or_404(student_id)
 
-@app.route('/<int:id>/delete', methods=['GET', 'POST'])
-
-def delete(id):
-    students = StudentModel.query.filter_by(id=id).first()
     if request.method == 'POST':
         if students:
-            db.session.delete(students)
+            db.session.delete(student)
             db.session.commit()
             return redirect('/')
             abort(404)
-        return render_template('delete.html')
+        return render_template('delete.html', student=student)
 
-app.run(host='localhost', port=5000)
-
-# -------------------------------------------------------------------------------
-# from flask import Flask, render_template, request, redirect
-# from models import db, StudentModel
-
-# app = Flask(__name__)
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db.init_app(app)
-
-# # Create the table before the first request
-# created_table = False
-
-# @app.before_request
-# def create_table():
-#     global created_table
-#     if not created_table:
-#         db.create_all()
-#         created_table = True
-
-# @app.route('/create', methods=['GET', 'POST'])
-# def create():
-#     if request.method == 'GET':
-#         return render_template('create.html')
-    
-#     if request.method == 'POST':
-#         hobby = request.form.getlist('hobbies')
-#         hobbies = ",".join(map(str, hobby))
-#         first_name = request.form['first_name']
-#         last_name = request.form['last_name']
-#         email = request.form['email']
-#         password = request.form['password']
-#         gender = request.form['gender']
-#         hobbies = hobbies
-#         country = request.form['country']
-
-#         students = StudentModel(
-#             first_name=first_name,
-#             last_name=last_name,
-#             email=email,
-#             password=password,
-#             gender=gender,
-#             hobbies=hobbies,
-#             country=country
-#         )
-#         db.session.add(students)
-#         db.session.commit()
-#         return redirect('/')
-    
-# @app.route('/', methods=['GET'])
-# def RetrieveList():
-#     students = StudentModel.query.all()
-#     return render_template('index.html', students=students)
-
-# if __name__ == "__main__":
-#     app.run(host='localhost', port=5000)
+if __name__ == "__main__":
+    app.run(host='localhost', port=8080)
 
 
